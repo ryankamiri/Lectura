cfed9e1dc95cb97ca9083783bb280644c9c13f25
Ryan <ryanamiri05@gmail.com>
2024-03-16 21:27:16 -0700

    Added loading to react

--- pawhacks-front/src/components/loading.js
+++ pawhacks-front/src/components/loading.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 export default function Loading() {
     return (
-        <h1>Loading</h1>
+        <div className="text-center position-absolute top-50 start-50 translate-middle">
+            <div
+                className="spinner-border text-light"
+                style={{width: "4rem", height: "4rem"}}
+                role="status"
+            >
+                <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="text-light mt-2">LOADING...</div>
+        </div>
     )
 }
